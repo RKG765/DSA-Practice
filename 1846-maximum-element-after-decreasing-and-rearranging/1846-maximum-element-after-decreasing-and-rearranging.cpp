b9226b32bb25b069fc17f389ef234a5fe1c065b3
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-06-29 02:14:21 +0530

    Time: 3 ms (94.4%), Space: 55.3 MB (8.68%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,13 +4,11 @@ public:
         int greatest = 1;
         sort(arr.begin(),arr.end());  //nlogn
 
-        if(arr[0]>1) arr[0]=1;   //first element always 1
+        arr[0]=1;   //first element always 1
 
         for(int i = 1;i<arr.size();i++){
-            int num;
             if(abs(arr[i]-arr[i-1])>1){
-                num = abs(arr[i]-arr[i-1]);
-                arr[i] = abs(arr[i]-num) +1;
+                arr[i] = arr[i-1]+1;
             }
             greatest = max(greatest,arr[i]);
         }
