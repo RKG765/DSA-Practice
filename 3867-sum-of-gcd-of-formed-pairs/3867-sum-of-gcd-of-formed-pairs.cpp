9412208081e9d507a7ae5978d1ac961adab48ca4
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-07-16 20:14:20 +0530

    Time: 52 ms (98.77%), Space: 148.6 MB (99.08%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
-        vector<int>prefixGcd;
+        //vector<int>prefixGcd;
         int n = nums.size();
 
         int maxEle = 0;
         for(int i = 0;i<n;i++){
             maxEle = max(maxEle,nums[i]);
 
-            prefixGcd.push_back(gcd(nums[i],maxEle));
+            // prefixGcd.push_back(gcd(nums[i],maxEle));
             // instead you can also update the value in the same place 
-            // nums[i] = gcd(maxEle,nums[i]);
+            nums[i] = gcd(maxEle,nums[i]);
             // it will reduce the s(0) = o(1) instead of o(n)
         }
 
-        sort(begin(prefixGcd),end(prefixGcd));
+        // sort(begin(prefixGcd),end(prefixGcd));
+        sort(begin(nums),end(nums));
         int s = 0;
-        int e = prefixGcd.size()-1;
+        // int e = prefixGcd.size()-1;
+        int e = nums.size()-1;
         long long res = 0;
         while(s<e){
-            long long curGcd = gcd(prefixGcd[s],prefixGcd[e]);
-            res += curGcd;
+            // long long curGcd = gcd(prefixGcd[s],prefixGcd[e]);
+            // res += curGcd;
+            res += gcd(nums[s],nums[e]);
             s++;
             e--;
         }
