4a7c668c424f4c995f32f87362d512094b6e9a6e
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-07-28 01:46:05 +0530

    Time: 0 ms (100%), Space: 13.4 MB (60.66%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -5,15 +5,14 @@ public:
         int max2 = -1;
         int n = nums.size();
 
-        for(int i=0;i<n;i++){
-            if(nums[i]>max1){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > max1) {
                 max2 = max1;
                 max1 = nums[i];
-            }
-            else if(nums[i]>=max2){
+            } else if (nums[i] >= max2) {
                 max2 = nums[i];
             }
         }
-        return (max1-1)*(max2-1);
+        return (max1 - 1) * (max2 - 1);
     }
 };
