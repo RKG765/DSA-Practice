0083fb030e18087ad7b874b07181c781b8fd47df
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-07-29 02:55:56 +0530

    Time: 122 ms (21.48%), Space: 70.5 MB (57.92%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,29 +1,28 @@
-#include<algorithm>  // for sort,reverse function
+#include <algorithm> // for sort,reverse function
 
 class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.length();
-        if(n==1){
+        if (n == 1) {
             return s;
         }
         string temp = "";
-        string rev = ""; // for the reverse of the string 
-        if(n%2==0){//even case
-            temp = s.substr(0,n/2);
-            sort(temp.begin(),temp.end());
+        string rev = "";  // for the reverse of the string
+        if (n % 2 == 0) { // even case
+            temp = s.substr(0, n / 2);
+            sort(temp.begin(), temp.end());
             rev = temp;
-            reverse(rev.begin(),rev.end());
-            temp+=rev;
-        }
-        else{//odd case
-            temp = s.substr(0,(n/2));
-            sort(temp.begin(),temp.end());
+            reverse(rev.begin(), rev.end());
+            temp += rev;
+        } else { // odd case
+            temp = s.substr(0, (n / 2));
+            sort(temp.begin(), temp.end());
             rev = temp;
-            int left_idx = (n/2); // 5/2=  2 but we need 3 one aswell 
+            int left_idx = (n / 2);
             temp += s[left_idx];
-            reverse(rev.begin(),rev.end());
-            temp+= rev;
+            reverse(rev.begin(), rev.end());
+            temp += rev;
         }
         return temp;
     }
