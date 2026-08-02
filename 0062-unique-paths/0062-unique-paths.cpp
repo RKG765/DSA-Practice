f1e7c88ddb883e47f599aebc4e6ddc25aeae8bfc
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-08-02 20:17:07 +0530

    Time: 3 ms (9.49%), Space: 9.6 MB (5.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,15 +6,15 @@ public:
         if(i>=m || j>=n) return 0;
         if(i==m-1 && j==n-1) return 1;
         if(mem[i][j]!=-1) return mem[i][j];
-        int res = 0;
+        //int res = 0;
 
-        for(auto &dir:dirs){
-            int i_  = i+dir[0];
-            int j_  = j+dir[1];
+        // for(auto &dir:dirs){
+        //     int i_  = i+dir[0];
+        //     int j_  = j+dir[1];
 
-            res += solve(i_,j_,m,n);
-        }
-        return mem[i][j] = res;
+        //     res += solve(i_,j_,m,n);
+        // }
+        return mem[i][j] = solve(i+1,j,m,n)+solve(i,j+1,m,n);
     }
     int uniquePaths(int m, int n) {
         mem.resize(m,vector<int>(n,-1));
