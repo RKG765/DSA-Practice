97834da317d1279fb428e3a7690e2b37f716292d
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-08-02 18:03:46 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> mem;
-    int solve(int left, int right, vector<int>& piles) {
-        if(left >= right) {
-            return 0;
-        }
-        if(mem[left][right] != -1)
-            return mem[left][right];
-        int leftSum = piles[left] + solve(left + 1, right, piles);
-        int rightSum = piles[right] + solve(left, right - 1, piles);
-
-        return mem[left][right] = max(leftSum, rightSum);
-    }
-
     bool stoneGame(vector<int>& piles) {
-        mem.resize(piles.size(), vector<int>(piles.size(), -1));
-        int AliceSum = solve(0, piles.size()-1, piles);
-        int totalSum = accumulate(piles.begin(), piles.end(), 0);
-        return AliceSum > totalSum - AliceSum;
+        return true;
     }
 };
