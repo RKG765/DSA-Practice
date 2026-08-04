cff1d7d0d6711742f3aa840534550fb5e7b09ba8
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-08-04 18:33:15 +0530

    Time: 0 ms (100%), Space: 14.9 MB (64%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,7 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+// BFS
+class Solution1 {
 public:
     vector<int> rightSideView(TreeNode* root) {
         if(root==NULL) return {};
@@ -30,6 +32,25 @@ public:
             }
             res.push_back(node->val);
         }
+        return res;
+    }
+};
+
+
+// DFS
+class Solution {
+public:
+    void preOrder(TreeNode *root,int level,vector<int>&res){
+        if(root==NULL) return;
+        if(res.size() < level){
+            res.push_back(root->val);
+        }
+        preOrder(root->right,level+1,res);
+        preOrder(root->left,level+1,res);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        preOrder(root,1,res);
         return res;
     }
 };
