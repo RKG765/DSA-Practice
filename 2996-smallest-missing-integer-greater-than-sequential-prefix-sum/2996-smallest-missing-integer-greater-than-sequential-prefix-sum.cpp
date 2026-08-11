988d2c56a1331d2ba9c712de94dd3b6320b42323
Raj Kumar Gupta <87042837+RKG765@users.noreply.github.com>
2026-08-11 17:55:48 +0530

    Time: 0 ms (100%), Space: 23.2 MB (6.61%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -3,24 +3,17 @@ public:
     int missingInteger(vector<int>& nums) {
         int preSeq = nums[0];
         int n = nums.size();
-
         for (int i = 1; i < n; i++) {
             if (nums[i] == (nums[i - 1] + 1)) {
                 preSeq += nums[i];
             } else
                 break;
         }
-        cout << "Prefix Sum: " << preSeq << endl;
-
+        // cout << "Prefix Sum: " << preSeq << endl;
         unordered_set<int> s;
-
         for (int i = 0; i < n; i++) {
             s.insert(nums[i]);
         }
-        for (int x : s) {
-            cout << x << endl;
-        }
-
         while (s.count(preSeq)) {
             preSeq++;
         }
